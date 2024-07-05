78ea6935040c95f733cee07bfcabcebd77cd4e7c
Akhmad Mustofa Solikin <91455464+mustofa08@users.noreply.github.com>
2024-07-05 21:08:57 +0700

    Update server.py

---
+++ server.py
@@ -0,0 +1,55 @@
+from socket import *
+import socket
+import threading
+import logging
+import datetime
+
+class ProcessTheClient(threading.Thread):
+	def __init__(self,connection,address):
+		self.connection = connection
+		self.address = address
+		threading.Thread.__init__(self)
+		# Memanggil konstruktor dari superclass
+
+	def run(self):
+		data_received = '' # untuk menyimpan data yang diterima
+		while True:
+			data = self.connection.recv(32)
+			data_received += data.decode()
+			if "\r\n\r\n" in data_received:
+				break
+			else: 
+				#Jika data kosong
+				break # keluar dari loop
+		if data_received.strip() == "TIME":
+			dataTime = datetime.datetime.now().strftime('%X')
+			dataTime += "\r\n\r\n" # Menambahkan delimiter
+			self.connection.sendall(dataTime.encode())
+
+		self.connection.close()
+
+class Server(threading.Thread):
+	def __init__(self):
+		self.the_clients = []
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+		# SOCK_STREAM digunakan untuk menentukan transport dengan TCP
+		threading.Thread.__init__(self)
+
+	def run(self):
+		self.my_socket.bind(('0.0.0.0',45000)) # Mengikat socket ke semua alamat IP yang tersedia pada port 45000
+		self.my_socket.listen(1)
+		while True:
+			self.connection, self.client_address = self.my_socket.accept()
+			logging.warning(f"connection from {self.client_address}")
+			
+			clt = ProcessTheClient(self.connection, self.client_address)
+			clt.start()
+			self.the_clients.append(clt)
+	
+
+def main():
+	svr = Server()
+	svr.start()
+
+if __name__=="__main__":
+	main()
